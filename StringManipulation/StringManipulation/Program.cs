65ef608061675fe67d6ced31a942cc3fde55225d
Rhea-Gomez <rhea.gomez@aurionpro.com>
2025-07-11 10:45:06 +0530

    Assignment on String manipulation

---
+++ StringManipulation/StringManipulation/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace StringManipulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string example = "Sachin,Aurionpro,Mumbai";
+            string[] words = example.Split(',');
+
+            Console.WriteLine("Developer name : " + words[0]);
+            Console.WriteLine("Company name : " + words[1]);
+            Console.WriteLine("Location : " + words[2]);
+        }
+    }
+}
